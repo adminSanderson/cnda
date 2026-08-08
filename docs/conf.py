8cd99b43875a5d975ee120b607fd031ac25599a4
adminSanderson <karelianalex@gmail.com>
2026-08-08 13:48:18 +0300

    docs: Add Sphinx configuration with Breathe integration

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'cnda'
+copyright = '2024, adminSanderson'
+author = 'adminSanderson'
+version = '1.0.0'
+
+extensions = ['breathe']
+
+breathe_projects = {
+    'cnda': './doxygen/xml'
+}
+breathe_default_project = 'cnda'
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
